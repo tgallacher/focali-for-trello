b9682689b563085673c570972d166db5cef92ad4
Tom Gallacher <tgallacher@users.noreply.github.com>
2019-06-03 20:20:14 +0100

    build: overwrite storybook file-loader so we can use our own

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,6 +10,12 @@ const projWebpack = require('../webpack.config');
 module.exports = ({ config, mode }) => {
   // Remove the default JS webpack rule
   config.module.rules.shift();
+
+  // remove storybook's file-loader config so we can use our own
+  config.module.rules = config.module.rules.filter(rule =>
+    'loader' in rule ? !rule.loader.includes('file-loader/dist/cjs.js') : true,
+  );
+
   // We don't need to parse the code entrypoint for Story book.
   // The relevant pieces will be manually imported within stories.
   delete projWebpack['entry'];
